558d584620c37c830bd50babf428d9851aa99b95
jessegrabowski <jessegrabowski@gmail.com>
2026-09-06 09:49:28 +0800

    Export the binding helpers and document the models subpackage

--- pytensor_ml/models/__init__.py
+++ pytensor_ml/models/__init__.py
@@ -2,6 +2,7 @@
 import pytensor_ml.models.clip  # registers the CLIP builders
 import pytensor_ml.models.gpt2  # registers the GPT-2 builder
 
+from pytensor_ml.models.binding import bind_layer_norm, bind_linear
 from pytensor_ml.models.keys import KeyMap, channels_last
 from pytensor_ml.models.registry import (
     architecture_name,
@@ -12,6 +13,8 @@ from pytensor_ml.models.registry import (
 __all__ = [
     "KeyMap",
     "architecture_name",
+    "bind_layer_norm",
+    "bind_linear",
     "build_from_config",
     "channels_last",
     "register_builder",
